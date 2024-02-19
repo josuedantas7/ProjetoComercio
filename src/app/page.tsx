01b37dcc82a29be8fcb3a773b256ac63f0ec865d
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-19 18:58:04 -0300

    feat: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import FormRegisterProduct from "./components/Form/FormRegisterProduct";
+import TableListProducts from "./components/Table/TableListProducts";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold mt-12">Cadastrar Produto</h1>
+      <div className="w-[900px] max-[960px]:w-[90%] mx-auto">
+        <FormRegisterProduct/>
+      </div>
+      <div className="mt-8 w-[900px] max-[960px]:w-[90%] mx-auto border rounded-lg p-5 shadow-lg border-zinc-500">
+        <TableListProducts/>
+      </div>
+    </div>
+  );
+}
