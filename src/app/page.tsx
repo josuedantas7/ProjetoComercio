34f959fb0199fde007e94c4a3c95d43a0ac2252c
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-23 13:02:26 -0300

    style: fix ButtonReloadPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TableListProducts from "./components/Table/TableListProducts";
 export default function Home() {
   return (
     <div className="relative">
-      <div className="flex justify-center items-center mt-12 gap-32 max-[570px]:gap-12">
+      <div className="w-[900px] relative flex mt-12 justify-center max-[960px]:w-[90%] mx-auto">
         <h1 className="text-2xl text-center font-bold">Cadastrar Produto</h1>
         <ButtonReloadPage/>
       </div>
